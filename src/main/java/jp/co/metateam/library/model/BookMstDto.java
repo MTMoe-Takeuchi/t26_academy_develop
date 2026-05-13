c8bde9d641cf05699ee58d70ab5d468bddb21948
MTMoe-Takeuchi <moe.takeuchi@metateam.co.jp>
2026-05-13 14:08:06 +0900

    try/catch文をif文に修正しました

--- src/main/java/jp/co/metateam/library/model/BookMstDto.java
+++ src/main/java/jp/co/metateam/library/model/BookMstDto.java
@@ -13,22 +13,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookMstDto {
-    
-    private Long id; 
-    
-    // ここから追加します　ISBNのバリデーション（必須、13桁）
+    private Long id;
+
+    // ここから追加します ISBNのバリデーション（必須、13桁）
     @NotEmpty(message = "ISBNは必須です")
-    @Size(max = 13, message = "ISBNは13字以下で入力してください")
+    @Size(max = 13, message = "ISBNは13字で入力してください")
     private String isbn;
 
-    // 追加　書籍名のバリデーション（必須、255字以下）
-    @NotEmpty(message = "書籍名は必須です" )
+    // 書籍名のバリデーション（必須、255字以下）
+    @NotEmpty(message = "書籍名は必須です")
     @Size(max = 255, message = "書籍名は255字以下で入力してください")
     private String title;
-    
     private Timestamp deletedAt;
-
     private BookMst bookMst;
 
-    //ここまで追加しました
+    // ここまで追加しました
 }
